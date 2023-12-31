b93eaf6247aaa5e26385723f6c771b41105365e3
HectorGarciaCovena <hectormgc.85@gmail.com>
2023-12-30 20:49:14 -0500

    nuevo commit

--- app/Controllers/RestPersonas.php
+++ app/Controllers/RestPersonas.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 use CodeIgniter\RESTful\ResourceController;
 use App\Models\PersonasModel;
 
+//Definición del Controlador y sus Propiedades
 class RestPersonas extends ResourceController
 {
     protected $modelName = 'App\Models\PersonasModel';
@@ -11,15 +12,18 @@ class RestPersonas extends ResourceController
 
     public function index()
     {
-        $model = new PersonasModel();
-        return $this->respond($model->findAll());
+        $model = new PersonasModel(); //Crea una instancia del modelo "PersonasModel"
+        return $this->respond($model->findAll()); //obtiene todos los registros
     }
 
-    public function show($id = null)
+    // Función que OBTIENE los elementos de la API
+    public function show($id = null) //Muestra un elemento específico de la API según su ID
     {
         $model = new PersonasModel();
         $data = $model->find($id);
 
+        //Si el registro existe, responde con la información obtenida; 
+        //de lo contrario, responde con un mensaje de error.
         if ($data) {
             return $this->respond($data);
         } else {
@@ -27,6 +31,7 @@ class RestPersonas extends ResourceController
         }
     }
 
+    // Función que INSERTA un elemento de la API
     public function create()
     {
         $model = new PersonasModel();
@@ -37,14 +42,15 @@ class RestPersonas extends ResourceController
             'correo' => $this->request->getPost('correo')
         ];
 
-        $data = $this->request->getJSON();
+        $data = $this->request->getJSON(); // Convierte la información obtenida en formato JSON
         
-        $model->insert($data);
+        $model->insert($data); //Inserta un nuevo registro
 
-        return $this->respondCreated($data);
+        return $this->respondCreated($data); 
         
     }
 
+    // Función que ACTUALIZA un elemento de la API
     public function update($id = null)
     {
         $model = new PersonasModel();
@@ -55,18 +61,19 @@ class RestPersonas extends ResourceController
             'correo' => $this->request->getRawInput('correo'),
         ];
 
-        $data = $this->request->getJSON();
+        $data = $this->request->getJSON(); // Convierte la información obtenida en formato JSON
 
-        $model->update($id, $data);
+        $model->update($id, $data); //Utiliza este método del modelo para ACTUALIZAR el registro
 
         return $this->respond($model->find($id));
     }
 
+    // Función que ELIMINA un elemento de la API
     public function delete($id = null)
     {
         $model = new PersonasModel();
         
-        $model->delete($id);
+        $model->delete($id); //Utiliza este método del modelo para ELIMINAR el registro
 
         return $this->respondDeleted(['id' => $id]);
     }
